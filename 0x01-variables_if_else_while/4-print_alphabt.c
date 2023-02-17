725a88bd6cfa8d4623609882b419b4301c1579e3
kags02 <irumvaleon@gmail.com>
2023-02-17 23:12:53 +0200

    printing all characters except e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 /**
-* main - determines if a number is positive,negative or zero.
-* return: always 0 (success)
-*/
+ * main - determines if a number is positive,negative or zero.
+ * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
        	char ch = 'a';
+
        	while (ch <= 'z')
-       	{
+	{
 		if ((ch != 'q') || (ch != 'e'))
+		{
 			putchar(ch);
+		}
 		ch++;
 	}
 	putchar('\n');
