36ae9758c7cacf0c1308dd3832d6d0a44e2309c0
kags02 <irumvaleon@gmail.com>
2023-02-18 00:24:28 +0200

    printing lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
+#include <stdio.h>
 /**
-* main - determines if a number is positive,negative or zero.
-* return: always 0 (success)
-*/
+ * main - determines if a number is positive,negative or zero.
+ * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-        char ch = 'z';
-        while (ch >= 'a')
-        {
-         putchar(ch);
-                ch--;
-        }
-        putchar('\n');
-        return (0);
+	char ch;
+	
+	ch = 'z';
+       	while (ch >= 'a')
+       	{
+	       	putchar(ch);
+	       	ch--;
+	}
+	putchar('\n');
+	return (0);
 }       
