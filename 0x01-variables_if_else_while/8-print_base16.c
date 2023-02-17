a8730b6cbe277825d24372a9dca81d9a956aa737
kags02 <irumvaleon@gmail.com>
2023-02-18 01:21:49 +0200

    a program that prints all the numbers of base 16 in lowercase, followed by a new line.

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 /**
-* main - determines if a number is positive,negative or zero.
-* return: always 0 (success)
-*/
+ * main - determines if a number is positive,negative or zero.
+ * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 char c;
 int d;
-c='a';
-d=0;
-while (d<0)
-{ putchar(d+'0');
+c = 'a';
+d = 0;
+while (d < 10)
+{ putchar(d + '0');
 d++;
 }
 while (c <= 'f')
