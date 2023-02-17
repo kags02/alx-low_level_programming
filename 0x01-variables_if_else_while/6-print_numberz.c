5de16219800bdef3321c1ff5ea1a9a391c9e26cf
kags02 <irumvaleon@gmail.com>
2023-02-17 23:46:41 +0200

    using putchar to print numbers from 0 to 10

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
 /**
-* main - determines if a number is positive,negative or zero.
-* return: always 0 (success)
-*/
+ * main - determines if a number is positive,negative or zero.
+ * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
-        int a=1;
-        while (a<= 10)
-        {
-                putchar(a);
-                        a++;
-        }
-        putchar ('\n');
-        return (0);
+        int a;
+	
+	a = 0;
+	while (a < 10)
+	{
+		putchar(a + '0');
+		a++;
+	}
+	putchar ('\n');
+	return (0);
 }
 
