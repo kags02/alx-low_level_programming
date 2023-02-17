4c10138e6784b2b183f8e8e1b897c79403c3c8db
kags02 <irumvaleon@gmail.com>
2023-02-17 23:07:31 +0200

    printing both lowercase letters and uppercase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 /**
-* main - determines if a number is positive,negative or zero.
-* return: always 0 (success)
-*/
+ * main - determines if a number is positive,negative or zero.
+ * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
 int main(void)
 {
 	char ch = 'a';
 	char cb = 'A';
+
 	while (ch <= 'z')
 	{
 		putchar(ch);
 		ch++;
 	}
-	while (cb >= 'A')
+	while (cb <= 'Z')
 	{
 		putchar(cb);
 		cb++;
