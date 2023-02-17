3a505b79aceb2e51962431fff01b0a5c4e716694
kags02 <irumvaleon@gmail.com>
2023-02-17 23:20:59 +0200

    printing all characters except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,11 +9,11 @@
 
 int main(void)
 {
-       	char ch = 'a';
-
-       	while (ch <= 'z')
+	char ch = 'a';
+	
+	while (ch <= 'z')
 	{
-		if ((ch != 'q') || (ch != 'e'))
+		if ((ch != 'q') && (ch != 'e'))
 		{
 			putchar(ch);
 		}
