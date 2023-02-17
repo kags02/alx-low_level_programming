ca19b820416c5b580d98e9ba8b22ca4752368d18
kags02 <irumvaleon@gmail.com>
2023-02-17 22:13:31 +0200

    checking the sign of a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,12 @@
 #include <time.h>
 /**
  * main - determines if a number is positive,negative or zero.
- * return: always 0 (success)*/
+ * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	int n;
