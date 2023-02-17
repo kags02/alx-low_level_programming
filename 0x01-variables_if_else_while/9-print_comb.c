d032c59e1d21b8b0c0dfbe52095942fa67a4303e
kags02 <irumvaleon@gmail.com>
2023-02-18 01:31:13 +0200

    a program that prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
+#include <stdio.h>
 /**
-* main - determines if a number is positive,negative or zero.
-* return: always 0 (success)
-*/
+ * main - determines if a number is positive,negative or zero.
+ * return: always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	int a;
-	for (a=0; a < 100; a++)
+
+	for (a = 0; a < 10; a++)
 	{
 		putchar(a + '0');
-		if (a<9)
+		if (a < 9)
 		{
 		putchar(',');
 		putchar(' ');
