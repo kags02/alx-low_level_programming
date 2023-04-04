5c9072083bd4e4ba68007e629e46b6eb06e987bd
kags02 <irumvaleon@gmail.com>
2023-04-05 01:22:36 +0200

    a function that prints all elements of a singly linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * listint_len - Returns the elements of the list.
+ * print_listint -  Returns the elements of the list.
  * @h: A pointer to the head of the listint_t.
  *
- * Return: The number of elements in list.
+ * Return:  The number of elements in list.
  */
+
 size_t print_listint(const listint_t *h)
 {
 	unsigned long int v = 0;
