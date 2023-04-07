403b6ba4abbe1170c5c2e969005a8419996570eb
kags02 <irumvaleon@gmail.com>
2023-04-07 10:48:23 +0200

    a function which changes binary to int

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - changes binary to integer.
+ * @b: pointer to binary.
+ * Return: the integer from binary and 0 if the input is not binary.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0, x = 0, f;
+
+	while (b[x] != '\0')
+	{
+		if (b[x] < '2')
+		{
+			f = b[x] - '0';
+			n = n * 2 + f;
+			x++;
+		}
+		else
+			return (0);
+	}
+return (n);
+}
