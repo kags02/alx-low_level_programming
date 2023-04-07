da878d416a8826d6397e65b3eb78c734e53a6652
kags02 <irumvaleon@gmail.com>
2023-04-07 19:20:28 +0200

    funcion protoypes

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,4 +2,6 @@
 #define MAIN_H
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+int get_bit(unsigned long int n, unsigned int index);
+int set_bit( unsigned long int *n, unsigned int index);
 #endif
