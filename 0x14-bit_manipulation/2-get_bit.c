edd41e6f3bfa63be9cb992c917c753d0e9c48dc6
kags02 <irumvaleon@gmail.com>
2023-04-09 15:39:49 +0200

    returns the value of bit at a given inedx

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,9 @@ int get_bit(unsigned long int n, unsigned int index)
 	unsigned int f = 0;
 	int d;
 
+	if (index > (sizeof(unsigned long int) * 8 - 1))
+		return (-1);
+
 	while (n > 0)
 	{
 		d = n % 2;
