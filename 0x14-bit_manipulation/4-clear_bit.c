2d50f11526a3a2daf8f9d55fe1dca57a6796aa18
kags02 <irumvaleon@gmail.com>
2023-04-10 14:56:25 +0200

    sets the value of bit to 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -4,7 +4,7 @@
  * @n: pointer to int n.
  * @index: the bit to change.
  * Return: -1 if there is an error.
- * else - return 1;
+ * else - return 1.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
