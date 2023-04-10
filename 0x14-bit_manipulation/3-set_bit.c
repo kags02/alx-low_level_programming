f15afea23a3ac516037fbc9279f26e9041ba72a1
kags02 <irumvaleon@gmail.com>
2023-04-10 14:21:34 +0200

    set the value of bit at given index to 1

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -7,36 +7,8 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned long int d, t = 1;
-	unsigned long int g = 0, r = 0;
-	unsigned long int f = 0, h = 0;
-	unsigned long int s;
-
-	s = *n;
-	while (s != 0)
-	{
-		d = s % 2;
-		g = d * t + g;
-		s = s / 2;
-		t = t * 10;
-		h++;
-	}
-	t = 1;
-	while (g > 0 || f <= index || index >= h)
-	{
-		d = g % 10;
-		if (f == index)
-			d = 1;
-		r = d * t + r;
-		g = g / 10;
-		t = t * 2;
-		if (index > h && f == index)
-		{
-			*n = r;
-			return (1);
-		}
-		f++;
-	}
-	*n = r;
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+	*n = *n | (1 << index);
 	return (1);
 }
