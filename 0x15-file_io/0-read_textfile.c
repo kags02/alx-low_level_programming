1edad05e73ea4b8259813a7095b9edddca7b123e
kags02 <irumvaleon@gmail.com>
2023-04-13 22:35:16 +0200

    reads text file and prints it to POSIX standard output i

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,9 +17,13 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (hello == NULL)
 	return (0);
 	a = open(filename, O_RDONLY);
+	if (a == -1)
+		return (0);
 	b = read(a, hello, letters);
+	if (b == -1)
+		return (0);
 	c = write(STDOUT_FILENO, hello, b);
-	if ( c!= b)
+	if (c == -1)
 		return (0);
 	free(hello);
 	close(a);
