183054ea0f52c0c3109a25f0a6d8cae74a757c36
kags02 <irumvaleon@gmail.com>
2023-04-15 00:15:05 +0200

    get argument in main function and  copy content from the first file to second

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -4,23 +4,30 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+/**
+ * cp - copy content from one file to another.
+ * @filename1: file to copy from.
+ * @filename2: file to cpy to.
+ * Return: void.
+ */
 void cp(const char *filename1, const char *filename2)
 {
-	int n,j,k,u;
-        char buffer[1024];
-        j = open(filename1, O_RDONLY);
+	int n, j, k, u;
+	char buffer[1024];
+
+	j = open(filename1, O_RDONLY);
 	if (j == -1)
 	{
-		dprintf(2,"Can't read from the file %s\n", filename1);
+		dprintf(2, "Can't read from the file %s\n", filename1);
 		exit(98);
 	}
 	n = open(filename2, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (n == -1)
 	{
 		exit(99);
-		dprintf(2,"Can't write to %s\n", filename2);
-	}	
-	k = read(j, buffer , 1024);
+		dprintf(2, "Can't write to %s\n", filename2);
+	}
+	k = read(j, buffer, 1024);
 	if (k == -1)
 		exit(-1);
 	u = write(n, buffer, k);
@@ -31,16 +38,22 @@ void cp(const char *filename1, const char *filename2)
 	exit(1);
 
 }
+/**
+ * main - receives arguments and test the above function.
+ * @b: number of arguments passed.
+ * @av: arguments passed.
+ * Return: 0.
+ */
 int main(int b, char **av)
 {
 
 	if (b != 3)
 	{
-		dprintf(2,"cp %s %s",av[1],av[2]);
+		dprintf(2, "cp %s %s", av[1], av[2]);
 		exit(97);
 	}
 	cp(av[1], av[2]);
-	return 0;
+	return (0);
 }
 
 
