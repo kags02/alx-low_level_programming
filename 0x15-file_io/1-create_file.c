7395fad47c219b23917b1d46ad0e96f347e0bfb6
kags02 <irumvaleon@gmail.com>
2023-04-14 23:46:39 +0200

    create file and write in it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,11 @@ int create_file(const char *filename, char *text_content)
 	c = 0;
 	if (filename == NULL)
 		return (-1);
-	while (text_content[c] != '\0')
-		c++;
+	if (text_content != NULL)
+	{
+		for (c = 0; text_content[c]; )
+			c++;
+	}
 	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (a == -1)
 		return (-1);
