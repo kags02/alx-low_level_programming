e495e4c5da2ec791a8f4576be4c0d8b20338e6d3
kags02 <irumvaleon@gmail.com>
2023-04-14 23:06:38 +0200

    append text to file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdlib.h>
+/**
+ * append_text_to_file - append text to file.
+ * @filename: name of file.
+ * @text_content: the text to append to file.
+ * Return: 1 on success and -1 on failure.
+ */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	char *bd;
@@ -13,15 +19,17 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (a == -1)
 		return (-1);
 	bd = malloc(sizeof(char) * c);
-c = 0;
-for ( ; text_content[c] != '\0'; c++)
-{
-	bd[c] = text_content[c];
-}
-d = write(a, bd, c);
-if (d == -1)
+	if (bd == NULL)
+		return (-1);
+	c = 0;
+	for ( ; text_content[c] != '\0'; c++)
+	{
+		bd[c] = text_content[c];
+	}
+	d = write(a, bd, c);
+	if (d == -1)
 	return (-1);
-close(a);
-free(bd);
-return (1);
+	close(a);
+	free(bd);
+	return (1);
 }
